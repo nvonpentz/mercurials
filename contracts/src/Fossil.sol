2ef499b0a64232155bc2e0b233010e6f5cc6d04a
nvonpentz <12549658+nvonpentz@users.noreply.github.com>
2023-01-16 23:57:50 -0500

    save

--- contracts/src/Fossil.sol
+++ contracts/src/Fossil.sol
@@ -54,13 +54,21 @@ contract Fossil {
     //     return generateRandom(0, 16777215, seed);
     // }
 
-    function generateFrequency(uint tokenId) public view returns (string memory) {
-        uint random = generateRandom(30, 99, tokenId);
+    function generateFrequency(uint tokenId, bool turbulenceType) public view returns (string memory) {
+        uint random;
+        if (turbulenceType) {
+            // Fractal noise
+            random = generateRandom(30, 99, tokenId);
+        } else {
+            console.log('turbulent noise freq');
+            // Turbulent noise
+            random = generateRandom(1, 60, tokenId);
+        }
         string memory frequency;
         if (random < 100) {
-            frequency = string.concat('0', random.toString());
+            frequency = string.concat('0.0', random.toString());
         } else if (random < 10) {
-            frequency = string.concat('00', random.toString());
+            frequency = string.concat('0.00', random.toString());
         } else {
             frequency = random.toString();
         }
@@ -71,16 +79,17 @@ contract Fossil {
     function generateOctaves(uint tokenId, bool turbulenceType) public view returns (string memory) {
         if (turbulenceType) {
             // Fractal noise
+            return generateRandom(1, 3, tokenId).toString();
         }
 
         // Turbulence
-        return generateRandom(1, 3, tokenId).toString();
+        return generateRandom(2, 5, tokenId).toString();
     }
 
     function generateScale(uint tokenId, bool turbulenceType) public view returns (string memory) {
         if (turbulenceType) {
             // Fractal noise
-            return generateRandom(10, 100, tokenId).toString();
+            return generateRandom(1, 50, tokenId).toString();
         }
         // Turbulence noise
         return generateRandom(0, 80, tokenId).toString();
@@ -102,16 +111,17 @@ contract Fossil {
     function generateSVG(uint seed) public view returns (string memory) {
         /* Color parameters*/
         string memory backgroundColor = generateRandomColor(seed);
-        console.log('backgroundColor', backgroundColor);
-        // string memory foregroundColor = generateRandomColor(seed+1);
-        // console.log('foregroundColor', foregroundColor);
-
-
+        console.log('background color: ', backgroundColor);
+        string memory foregroundColor = generateRandomColor(seed+1);
+        // string memory lightingColor = generateRandomColor(seed+2);
+        string memory lightingColor = '#ffffff';
 
         /* Filter parameters */
-        bool turbulenceTypeBool = seed % 2 == 0; 
+        // bool turbulenceTypeBool = seed % 2 == 0; 
+        bool turbulenceTypeBool = true;
         string memory turbulenceType = turbulenceTypeBool ? "fractalNoise" : "turbulence";
-        string memory frequency = generateFrequency(seed);
+        string memory frequency = generateFrequency(seed, turbulenceTypeBool);
+        console.log('frequency', frequency);
         string memory scale = generateScale(seed, turbulenceTypeBool);
         string memory octaves = generateOctaves(seed, turbulenceTypeBool);
         console.log('turblenceType:', turbulenceType);
@@ -125,22 +135,22 @@ contract Fossil {
                 '<svg width="500" height="500" version="1.1" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">',
                   '<defs>',
                     '<filter id="cracked-lava">',
-                      // '<feFlood flood-color="', foregroundColor,'" result="r15" />',
-                      '<feFlood flood-color="gray" result="r15" />',
-                      '<feTurbulence baseFrequency="0.', frequency, '" type="', turbulenceType, '" numOctaves="', octaves ,'" result="r1" />',
-                      '<feDisplacementMap result="r5" xChannelSelector="R" in2="r1" in="r1" yChannelSelector="G" scale="', scale, '"> </feDisplacementMap>',
+                      '<feFlood flood-color="', backgroundColor,'" result="r15"/>',
+                      '<feTurbulence baseFrequency="', frequency,'" type="fractalNoise" numOctaves="2" result="r1" />',
+                      '<feDisplacementMap result="r5" xChannelSelector="R" in2="r1" in="r1" yChannelSelector="G" scale="', scale,'" baseFrequency="0.041" />',
                       '<feComposite result="r2" operator="in" in="r15" in2="r5" />',
-                      '<feSpecularLighting lighting-color="#ffffff" surfaceScale="-3.10131121" result="r4" specularConstant="2.13708425" specularExponent="15.19753456" in="r2"> <feDistantLight elevation="60" azimuth="0"> </feDistantLight> </feSpecularLighting>',
-                      '<feComposite k1="0.3874092" k3="1" k2="-0.5" in2="r2" in="r4" operator="arithmetic" result="r91" k4="0" />',
-                      '<feComposite in="r91" result="r4" operator="arithmetic" k2="2" k3="3.9779434" in2="r91" k1="0" k4="-0.57127724" />',
-                      '<feFlood result="result2" flood-color="', backgroundColor, '" flood-opacity="1" />',
-                      '<feComposite in2="result2" in="r4" operator="over" />',
+                      '<feSpecularLighting lighting-color="#ffffff" surfaceScale="-3.10131121" result="r4" specularConstant="2.13708425" specularExponent="15.19753456" in="r2">',
+                        '<feDistantLight elevation="60" azimuth="0"/>',
+                      '</feSpecularLighting>',
+                      '<feComposite k1="0.5" k3="1" k2="-0.5" in2="r2" in="r4" operator="arithmetic" result="r91" k4="0" />',
+                      '<feComposite in="r91" result="r4" operator="arithmetic" k2="2" k3="4" in2="r91" k1="0" k4="-0.5" id="feComposite949" />',
+                      '<feFlood flood-color="', foregroundColor, '" result="result1" />',
+                      '<feBlend mode="normal" in2="result1" in="r4" />',
                     '</filter>',
                   '</defs>',
                   '<rect width="500" height="500" filter="url(#cracked-lava)" />',
-                  '<rect width="50" height="50" x="0" fill="', backgroundColor, '" />',
-                  // '<rect width="50" height="50" x="50" fill="', foregroundColor, '" />',
-                '</svg>'
-            );
+                  '<rect fill="',backgroundColor,'" width="50" height="50" x="0"/>',
+                  '<rect fill="',foregroundColor,'" width="50" height="50" x="50"/>'
+                '</svg>');
     }
 }
