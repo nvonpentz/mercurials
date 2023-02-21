f91963930975eca72a817b3a377a3bb91532d1bd
nvonpentz <12549658+nvonpentz@users.noreply.github.com>
2023-02-20 20:02:05 -0500

    work on ui. it's getting there.

--- contracts/test/Fossil.t.sol
+++ contracts/test/Fossil.t.sol
@@ -16,30 +16,9 @@ contract FossilTest is Test {
 
     receive() external payable {}
 
-    // function testGenerateRandom2() public {
-    //     uint seed = 0;
-    //     uint nonce = 0;
-    //     uint rand1;
-    //     uint rand2;
-    //     (rand1, nonce) = fossil.generateRandom(0, 3, seed, nonce);
-    //     assertEq(nonce, 1);
-    //     (rand2, nonce) = fossil.generateRandom(0, 3, seed, nonce);
-    //     assertEq(nonce, 2);
-    //     assertTrue(rand1 != rand2);
-    // }
-
     function testGenerateSVGOnce() public view {
         fossil.generateSVG(0);
     }
-
-    // function testGenerateSVGManyTimes() public {
-    //     // call generateSVG(x) once for every x from 0..10000
-    //     for (uint i = 0; i < 10000; i++) {
-    //         string memory svg = fossil.generateSVG(i);
-    //         // check that the SVG is valid
-    //         assertTrue(bytes(svg).length > 0);
-    //     }
-    // }
 
     // Token tests
     function testNextToken() public {
