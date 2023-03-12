74028b64f018990f53b82c83c48642c543228b46
nvonpentz <12549658+nvonpentz@users.noreply.github.com>
2023-03-12 15:33:19 -0400

    Revert other changes to config.tsx that do selectively import since it doesn't matter any more

--- utils/config.tsx
+++ utils/config.tsx
@@ -1,15 +1,7 @@
+const foundryAddress = require("../contracts/deploys/mercurial.31337.address.json").address;
+const foundryAbi = require("../contracts/deploys/mercurial.31337.compilerOutput.json").abi;
 const goerliAddress = require("../contracts/deploys/mercurial.5.address.json").address;
 const goerliAbi = require("../contracts/deploys/mercurial.5.compilerOutput.json").abi;
-
-let foundryAddress;
-let foundryAbi;
-
-// 31337 chain ID deployments are only supported in development
-// because the configs are not tracked by git
-if (process.env.NEXT_PUBLIC_ENV === "development") {
-  foundryAddress = require("../contracts/deploys/mercurial.31337.address.json").address;
-  foundryAbi = require("../contracts/deploys/mercurial.31337.compilerOutput.json").abi;
-}
 
 interface Deployments {
   [chainId: string]: {
