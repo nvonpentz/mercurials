da6b762bbbbd07ccd09c01835516c06007ed47af
nvonpentz <12549658+nvonpentz@users.noreply.github.com>
2023-03-28 09:04:46 -0400

    Fix loading of Lato font by adding _document.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -105,12 +105,6 @@ const Home: NextPage = () => {
           <meta name="description" content="TODO" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <Navbar />
         <main className={styles.main}>
