81df8e6c2fee6c698b47d3ac0dca88c7c7fb0484
nvonpentz <12549658+nvonpentz@users.noreply.github.com>
2023-03-28 19:42:06 -0400

    Try explicitly adding 700 weight Lato

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
+            href="https://fonts.googleapis.com/css2?Lato:wght@400;700&display=swap"
             rel="stylesheet"
           />
         </Head>
