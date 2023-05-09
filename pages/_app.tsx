3222410e667c58ff958e2f7c8cbea1b25b36404a
nvonpentz <12549658+nvonpentz@users.noreply.github.com>
2023-05-08 20:46:20 -0400

    Add polygon network

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,14 @@ import {
   getDefaultWallets,
 } from "@rainbow-me/rainbowkit";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { goerli, foundry } from "wagmi/chains";
+import { goerli, polygon, foundry } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import { publicProvider } from "wagmi/providers/public";
 import { useEffect, useState } from "react";
 
 const { chains, provider, webSocketProvider } = configureChains(
-  process.env.NEXT_PUBLIC_ENV === "production" ? [goerli] : [goerli, foundry],
+  process.env.NEXT_PUBLIC_ENV === "production" ? [goerli] : [goerli, polygon, foundry],
   [
     alchemyProvider({
       apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY ?? "",
--- utils/config.js
+++ utils/config.js
@@ -2,6 +2,8 @@ import { address as foundryAddress } from "../contracts/deploys/mercurial.31337.
 import { abi as foundryAbi } from "../contracts/deploys/mercurial.31337.compilerOutput.json";
 import { address as goerliAddress } from "../contracts/deploys/mercurial.5.address.json";
 import { abi as goerliAbi } from "../contracts/deploys/mercurial.5.compilerOutput.json";
+import { address as polygonAddress } from "../contracts/deploys/mercurial.137.address.json";
+import { abi as polygonAbi } from "../contracts/deploys/mercurial.137.compilerOutput.json";
 
 export const deployments = {
   31337: {
@@ -11,5 +13,9 @@ export const deployments = {
   5: {
     address: goerliAddress,
     abi: goerliAbi,
-  }
+  },
+  137: {
+    address: polygonAddress,
+    abi: polygonAbi,
+  },
 };
