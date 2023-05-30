dc56d59406b0b6b5c546ce5ee53a7f9c8c584578
nvonpentz <12549658+nvonpentz@users.noreply.github.com>
2023-05-30 19:35:31 -0400

    Fix utils/config.tsx / .js

---
+++ utils/config.tsx
@@ -0,0 +1,22 @@
+const foundryAddress = require("../contracts/deploys/mercurials.31337.address.json").address;
+const foundryAbi = require("../contracts/deploys/mercurials.31337.compilerOutput.json").abi;
+const goerliAddress = require("../contracts/deploys/mercurials.5.address.json").address;
+const goerliAbi = require("../contracts/deploys/mercurials.5.compilerOutput.json").abi;
+
+interface Deployments {
+  [chainId: string]: {
+    address: string;
+    abi: any;
+  };
+}
+
+export const deployments: Deployments = {
+  31337: {
+    address: foundryAddress,
+    abi: foundryAbi,
+  },
+  5: {
+    address: goerliAddress,
+    abi: goerliAbi,
+  }
+};
