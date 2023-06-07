fdef22615562d605d26cabc6aae33bc15e9737b5
nvonpentz <12549658+nvonpentz@users.noreply.github.com>
2023-06-06 20:58:39 -0400

    Disable foundry

--- utils/config.tsx
+++ utils/config.tsx
@@ -1,5 +1,5 @@
-const foundryAddress = require("../contracts/deploys/mercurials.31337.address.json").address;
-const foundryAbi = require("../contracts/deploys/mercurials.31337.compilerOutput.json").abi;
+// const foundryAddress = require("../contracts/deploys/mercurials.31337.address.json").address;
+// const foundryAbi = require("../contracts/deploys/mercurials.31337.compilerOutput.json").abi;
 const goerliAddress = require("../contracts/deploys/mercurials.5.address.json").address;
 const goerliAbi = require("../contracts/deploys/mercurials.5.compilerOutput.json").abi;
 
@@ -11,10 +11,10 @@ interface Deployments {
 }
 
 export const deployments: Deployments = {
-  31337: {
-    address: foundryAddress,
-    abi: foundryAbi,
-  },
+  // 31337: {
+  //   address: foundryAddress,
+  //   abi: foundryAbi,
+  // },
   5: {
     address: goerliAddress,
     abi: goerliAbi,
